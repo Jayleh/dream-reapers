1fd6110a5ad730476483ad3d57d70cade2f7ed87
Jayleh <jaylehyun@gmail.com>
2018-03-12 23:43:47 -0700

    extracted movie data, can grab movie data that have been recently release, need to figure out rate limtits

--- movie_sentiment.py
+++ movie_sentiment.py
@@ -13,24 +13,14 @@ from config import (consumer_key, consumer_secret,
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(),
+                 wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 # Initialize Sentiment Analyzer
 analyzer = SentimentIntensityAnalyzer()
 
-search_term = "Black Panther"
-
-public_tweets = api.search(search_term,
-                           count=25,
-                           until="2018-03-02")
-
-public_tweets
-
-# for tweet in public_tweets['statuses']:
-#     print(tweet['text'])
-#     print(tweet['created_at'])
 # Target user
-search_term = "Black Panther"
+search_term = "a wrinkle in time"
 
 # Create variable for holding the oldest tweet
 oldest_tweet = None
@@ -38,41 +28,68 @@ oldest_tweet = None
 # Create list of dictionaries
 sentiment = []
 
-# Get all tweets from home feed (for each page specified)
-public_tweets = api.search(search_term,
-                           count=25,
-                           lang='en',
-                           result_type="recent",
-                           until="2018-03-01",
-                           max_id=oldest_tweet)
+# "Real Person" Filters
+min_tweets = 5
+max_tweets = 10000
+max_followers = 2500
+max_following = 2500
+lang = "en"
 
-# Loop through all tweets
-for tweet in public_tweets['statuses']:
+# Date
+search_date = "2018-03-08"
 
-    # Grab tweet data
-    name = tweet['user']['screen_name']
-    tweet_text = tweet['text']
-    date = tweet['created_at']
+# Loop through 500 times (total of 500000 tweets)
+for x in range(500):
 
-    # Run Vader Analysis on each tweet
-    results = analyzer.polarity_scores(tweet["text"])
-    compound = results["compound"]
-    positive = results['pos']
-    neutral = results['neu']
-    negative = results['neg']
+    # Get all tweets from home feed (for each page specified)
+    public_tweets = api.search(search_term,
+                               count=1000,
+                               lang='en',
+                               until=search_date,
+                               max_id=oldest_tweet)
 
-    # Create dictionary holding tweet data
-    tweet_dict = {'Handle': name, 'Tweet': tweet_text, 'Date': date, 'Compound': compound,
-                  'Positive': positive, 'Neutral': neutral, 'Negative': negative}
+    # Loop through all tweets
+    for tweet in public_tweets['statuses']:
 
-    # Append tweet data to sentiment list
-    sentiment.append(tweet_dict)
+        # Use filters to check if user meets conditions
+        if (tweet["user"]["followers_count"] < max_followers and
+            tweet["user"]["statuses_count"] > min_tweets and
+            tweet["user"]["statuses_count"] < max_tweets and
+            tweet["user"]["friends_count"] < max_following and
+                tweet["user"]["lang"] == lang):
 
-    # Reassign the the oldest tweet (i.e. the max_id)
-    oldest_tweet = int(tweet["id_str"])
+            # Grab tweet data
+            name = tweet['user']['screen_name']
+            tweet_text = tweet['text']
+            date = tweet['created_at']
 
-    # Subtract 1 so the previous oldest isn't included
-    # in the new search
-    oldest_tweet = oldest_tweet - 1
+            # Run Vader Analysis on each tweet
+            results = analyzer.polarity_scores(tweet["text"])
+            compound = results["compound"]
+            positive = results['pos']
+            neutral = results['neu']
+            negative = results['neg']
 
-pprint(sentiment)
+            # Create dictionary holding tweet data
+            tweet_dict = {'Handle': name, 'Tweet': tweet_text, 'Date': date, 'Compound': compound,
+                          'Positive': positive, 'Neutral': neutral, 'Negative': negative}
+
+            # Append tweet data to sentiment list
+            sentiment.append(tweet_dict)
+
+            # Reassign the the oldest tweet (i.e. the max_id)
+            oldest_tweet = int(tweet["id_str"])
+
+            # Subtract 1 so the previous oldest isn't included
+            # in the new search
+            oldest_tweet = oldest_tweet - 1
+
+# Create dataframe
+awit_tweets = pd.DataFrame(sentiment)
+
+# Reorder columns
+awit_tweets = awit_tweets.iloc[:, [2, 6, 1, 0, 5, 4, 3]]
+
+# Save to csv
+awit_tweets.to_csv(f'sent_data/{search_date}_AWIT_sentiment.csv', encoding='utf-8', index=False)
+awit_tweets
