b2baed500741012f420899f6700e9e3778c0fda9
Jayleh <jaylehyun@gmail.com>
2018-03-15 14:15:16 -0700

    created python files for notebooks

--- all_movie_sentiment.py
+++ all_movie_sentiment.py
@@ -1,10 +1,7 @@
 # Dependencies
-import json
-from pprint import pprint
 from datetime import datetime as dt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import tweepy
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from config import (consumer_key, consumer_secret,
@@ -19,82 +16,94 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(),
 # Initialize Sentiment Analyzer
 analyzer = SentimentIntensityAnalyzer()
 
-# Target user
-search_term = input("What do you want to search for? ")
+# Movie csv path
+csv_path = 'box_office_data/moviesFinal.csv'
 
-# Search until date
-search_date = input("Do you want to specify a date? ").lower()
+# Read csv
+movie_df = pd.read_csv(csv_path)
 
-if search_date == "no":
-    pass
-elif search_date == "yes":
-    search_date = input("At what date? Please use the fomat, YYYY-MM-DD. ")
-
-# Create variable for holding the oldest tweet
-oldest_tweet = None
+# # "Real Person" Filters
+# min_tweets = 5
+# max_tweets = 10000
+# max_followers = 2500
+# max_following = 2500
+# lang = "en"
 
 # Create list of dictionaries
 sentiment = []
 
-# "Real Person" Filters
-min_tweets = 5
-max_tweets = 10000
-max_followers = 2500
-max_following = 2500
-lang = "en"
+for title in movie_df['title']:
 
-# Loop through 18 times (total of 18000 tweets)
-for x in range(1):
+    # Assign title as search term
+    search_term = title
 
-    # Get all tweets from home feed (for each page specified)
-    public_tweets = api.search(search_term,
-                               count=100,
-                               lang='en',
-                               until=search_date,
-                               max_id=oldest_tweet)
+    # Create variable for holding the oldest tweet
+    oldest_tweet = None
 
-    # Loop through all tweets
-    for tweet in public_tweets['statuses']:
+    # List to hold average compound values for each movie
+    compound_list = []
 
-        # Use filters to check if user meets conditions
-        if (tweet["user"]["followers_count"] < max_followers and
-            tweet["user"]["statuses_count"] > min_tweets and
-            tweet["user"]["statuses_count"] < max_tweets and
-            tweet["user"]["friends_count"] < max_following and
-                tweet["user"]["lang"] == lang):
+    try:
+
+        # Get all tweets from home feed (for each page specified)
+        public_tweets = api.search(search_term,
+                                   count=100,
+                                   lang='en',
+                                   result_type="recent",
+                                   max_id=oldest_tweet)
+
+        # Loop through all tweets
+        for tweet in public_tweets['statuses']:
+
+            # Use filters to check if user meets conditions
+            # if (tweet["user"]["followers_count"] < max_followers and
+                # tweet["user"]["statuses_count"] > min_tweets and
+                # tweet["user"]["statuses_count"] < max_tweets and
+                # tweet["user"]["friends_count"] < max_following and
+                # tweet["user"]["lang"] == lang):
 
             # Grab tweet data
-            name = tweet['user']['screen_name']
             tweet_text = tweet['text']
-            date = tweet['created_at']
 
             # Run Vader Analysis on each tweet
             results = analyzer.polarity_scores(tweet["text"])
             compound = results["compound"]
-            positive = results['pos']
-            neutral = results['neu']
-            negative = results['neg']
 
-            # Create dictionary holding tweet data
-            tweet_dict = {'Handle': name, 'Tweet': tweet_text, 'Date': date, 'Compound': compound,
-                          'Positive': positive, 'Neutral': neutral, 'Negative': negative}
-
-            # Append tweet data to sentiment list
-            sentiment.append(tweet_dict)
+            # Append compound value to list
+            compound_list.append(compound)
 
             # Reassign the the oldest tweet (i.e. the max_id)
             oldest_tweet = int(tweet["id_str"])
 
             # Subtract 1 so the previous oldest isn't included
             # in the new search
-            oldest_tweet = oldest_tweet - 1
+            oldest_tweet -= 1
+
+        # Store average
+        tweet_dict = {"title": search_term,
+                      "compound": np.mean(compound_list),
+                      "tweet_count": len(compound_list)}
+
+        # Append tweet data to sentiment list
+        sentiment.append(tweet_dict)
+
+    except Exception as e:
+        print(e)
 
 # Create dataframe
-awit_tweets = pd.DataFrame(sentiment)
+movie_sent_df = pd.DataFrame(sentiment)
 
 # Reorder columns
-awit_tweets = awit_tweets.iloc[:, [2, 6, 1, 0, 5, 4, 3]]
+movie_sent_df = movie_sent_df[["title", "compound", "tweet_count"]]
+
+# Today's date
+today = dt.strftime(dt.now(), "%Y-%m-%d")
 
 # Save to csv
-awit_tweets.to_csv(f'sent_data/{search_date}_AWIT_sentiment.csv', encoding='utf-8', index=False)
-awit_tweets
+# movie_sent_df.to_csv(f'all_movie_data/{today}_all_movie_sent.csv', encoding='utf-8', index=False)
+
+# Merge dataframes
+merged_df = pd.merge(movie_df, movie_sent_df, how="left")
+
+# Save to csv
+# merged_df.to_csv(f'all_movie_data/{today}_all_movie_data.csv', encoding='utf-8', index=False)
---
+++ predict_movie_sentiment.py
@@ -0,0 +1,112 @@
+# Dependencies
+import json
+from pprint import pprint
+from datetime import datetime as dt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tweepy
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from config import (consumer_key, consumer_secret,
+                    access_token, access_token_secret)
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser(),
+                 wait_on_rate_limit=False, wait_on_rate_limit_notify=False)
+
+# Initialize Sentiment Analyzer
+analyzer = SentimentIntensityAnalyzer()
+
+# Movie list to predict success
+movie_list = ["#Gringo", "#TheHurricaneHeist", "#PreyAtNight", "#WrinkleInTime", "#LoveSimon",
+              "#TombRaider", "#PacificRimUprising", "#SherlockGnomes", "#Acrimony",
+              "#ReadyPlayerOne"]
+
+# User input to specify search until date
+search_date = input(
+    "What date do you want to query the movies? Has to be in this format (%Y-%m-%d): ")
+
+# "Real Person" Filters
+min_tweets = 5
+max_tweets = 10000
+max_followers = 2500
+max_following = 2500
+lang = "en"
+
+# Create list of dictionaries
+sentiment = []
+
+# Analyze each movie in list
+for movie in movie_list:
+
+    # Assign title as search term
+    search_term = movie
+
+    # Create variable for holding the oldest tweet
+    oldest_tweet = None
+
+    # List to hold average compound values for each movie
+    compound_list = []
+
+    try:
+
+        # Loop through 18 times (total of 1800 tweets)
+        for x in range(18):
+
+            # Get all tweets from home feed (for each page specified)
+            public_tweets = api.search(search_term,
+                                       count=100,
+                                       lang='en',
+                                       until=search_date,
+                                       max_id=oldest_tweet)
+
+            # Loop through all tweets
+            for tweet in public_tweets['statuses']:
+
+                # Use filters to check if user meets conditions
+                if (tweet["user"]["followers_count"] < max_followers and
+                    tweet["user"]["statuses_count"] > min_tweets and
+                    tweet["user"]["statuses_count"] < max_tweets and
+                    tweet["user"]["friends_count"] < max_following and
+                        tweet["user"]["lang"] == lang):
+
+                    # Grab tweet data
+                    tweet_text = tweet['text']
+
+                    # Run Vader Analysis on each tweet
+                    results = analyzer.polarity_scores(tweet["text"])
+                    compound = results["compound"]
+
+                    # Append compound value to list
+                    compound_list.append(compound)
+
+                    # Reassign the the oldest tweet (i.e. the max_id)
+                    oldest_tweet = int(tweet["id_str"])
+
+                    # Subtract 1 so the previous oldest isn't included
+                    # in the new search
+                    oldest_tweet -= 1
+
+        # Store average
+        tweet_dict = {"Movie Title": search_term,
+                      "Search Date": search_date,
+                      "Compound": np.mean(compound_list),
+                      "Tweet Count": len(compound_list)}
+
+        # Append tweet data to sentiment list
+        sentiment.append(tweet_dict)
+
+    except Exception as e:
+        print(e)
+
+# Create Dataframe
+movie_sent_df = pd.DataFrame(sentiment)
+
+# Reorder columns
+movie_sent_df = movie_sent_df.iloc[:, [1, 0, 2, 3]]
+
+# Save to csv
+# movie_sent_df.to_csv(f'prediction_data/{search_date}_movie_sentiment.csv',
+#                      encoding='utf-8', index=False)
