fcb64f6d7752fcc31f8bb2aabe06083c8998cdae
mcyee90 <mcyee90@gmail.com>
2018-03-16 18:14:45 -0700

    update data scrub, add graphing, graphs, input

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 omdb_key = "e8913e4c"
+
+
+ConsumerKey = "8EYPuk7R90KmWudp2AH6q26lH"
+ConsumerSecret = "2KBNXgHgnxoyVVHK1Zq4tr04rlCoy2dS5wMbRCrrFcRizvWJDE"
+AccessToken = "971211790443933696-Hlc12mexv3tvnJGHyUS1KwpZreI6byh"
+AccessTokenSecret = "sQyviLrgPJj5OcY2qvj8oCqi6pZBcYXvjlrtcCBgoFIUQ"
